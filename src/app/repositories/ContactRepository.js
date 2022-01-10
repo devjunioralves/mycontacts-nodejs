0bfb645a9231dab750b888ffdf917bdd1f6d6a32
Junior Alves <junior@boxti.com.br>
2022-01-10 19:56:48 -0300

    feat: Refatora métodos CRUD com express e PG

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,84 +1,60 @@
 /* eslint-disable no-promise-executor-return */
 const { v4 } = require('uuid');
 
-let contacts = [
-  {
-    id: v4(),
-    name: 'Junior',
-    email: 'junior@gmail.com',
-    phone: '47997220531',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@gmail.com',
-    phone: '47997220531',
-    category_id: v4(),
-  },
-];
+const db = require('../../database');
 
 class ContactRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    return rows
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
-  }
-
-  create(
+  async create(
     {
       name, email, phone, category_id,
-    },
-  ) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    }
+    ) {
+      const [row] = await db.query(`
+      INSERT INTO contacts(name, email, phone, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `, [name, email, phone, category_id]);
+
+      return row
+    }
+
+    async update(id, {
+      name, email, phone, category_id,
+    }) {
+      const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1,
+      email = $2,
+      phone = $3,
+      category_id = $4
+      WHERE id = $5
+      RETURNING *`, [name, email, phone, category_id, id]);
+
+      return row;
+    }
+
+    async delete(id) {
+      const deleteOp = await db.query(`DELETE FROM contacts WHERE id = $1`, [id]);
+
+      return deleteOp
+    }
   }
-
-  update(id, {
-    name, email, phone, category_id,
-  }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-
-      resolve(updatedContact);
-    });
-  }
-}
 
 module.exports = new ContactRepository();
